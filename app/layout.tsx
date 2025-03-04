e798f1ff665369c3ed38c8e2801b2b24e64de66b
SkyVence <xormine.1200@gmail.com>
2025-03-04 19:25:23 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={inter.className}>
-      import { Analytics } from "@vercel/analytics/react"{children}</body>
+        <Analytics/>
+        {children}
+        </body>
     </html>
   )
 }
